4f73e3ed6d3a3ed34db95426002d7666e5993a60
MisterPotter <tylerjpotter@shaw.ca>
2015-02-18 18:36:44 -0800

    Jira creation times and resolution times are now being stored as datetime objects, and are being subtracted successfully

--- SEng_371_Python_Script.py/sonar.py
+++ SEng_371_Python_Script.py/sonar.py
@@ -19,33 +19,23 @@ def main():
     issue = jira.issue('SONAR-5132')
 
     # print issue.raw
-
-    print issue.fields.created
-    start_time = issue.fields.created
-
-    print issue.fields.resolutiondate
-    end_time = issue.fields.resolutiondate
+    get_date_difference(issue)
 
 
 # Format Expected: 2014-04-22T05:06:50.714-0500
-"""def create_date(date_string):
+def get_date_difference(issue):
 
-    date_time_array = date_string.split("T")
-    dates_array = date_time_array[0].split("-")
-    time_array = date_time_array[1].split(";")
+    # Not accounting for any time smaller than seconds
+    start_date_string = issue.fields.created.split(".")[0]
+    end_date_string = issue.fields.resolutiondate.split(".")[0]
 
-    # Create a readable array of all dates
-    dates = {
-        "year": dates_array[0],
-        "month": dates_array[1],
-        "day": dates_array[2]
-    }
+    date_format = "%Y-%m-%dT%H:%M:%S"
 
-    # Create a readable array of all times up to seconds
-    times = {
-        "hour": time_array[0],
-        "minute":
-    }"""
+    # Create the datetime objects for easy subtraction
+    start_date_time = datetime.strptime(start_date_string, date_format)
+    end_date_time = datetime.strptime(end_date_string, date_format)
+
+    print end_date_time - start_date_time
 
 
 if __name__ == '__main__':
